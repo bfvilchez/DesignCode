4076154b30983cb2ee90e2b4741836814bffe429
Brian <bfvilchez@icloud.com>
2020-09-13 19:42:27 -0600

    Created SwiftUI Project

---
+++ DesignCode(iOS14)/Shared/DesignCode_iOS14_App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode_iOS14_App.swift
+//  Shared
+//
+//  Created by brian vilchez on 9/13/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode_iOS14_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
